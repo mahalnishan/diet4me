f40d91f864b7dd5ab1b6b97b174589d9b1b69757
mahalnishan <nmahal96@gmail.com>
2025-09-15 01:35:52 -0400

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+// app/head.tsx
+export default function Head() {
+    return (
+      <>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* Optional helpful meta */}
+        <meta name="format-detection" content="telephone=no" />
+        <title>Diet4Me</title>
+      </>
+    );
+  }
+  
